fcb7016684986c7beb2db982a145da9b5091fff6
HolaholitaDavid <holaholitadavid@gmail.com>
2023-01-20 15:05:04 +0100

    exemple senzill working

--- src/coroutine.js
+++ src/coroutine.js
@@ -3,24 +3,60 @@ function callcc(f) {
     return f(kont);
 }
 
-function resume(where, value) {
-
-
-}
 
 
 function make_coroutine(aResumer) {
-
     let firstTime = true;
-    let savedContinuation = nil;
+    let saved_continuation = new Continuation();
 
+    function resume(nom_corutina, value) {
+        
+        saved_continuation = new Continuation(value);
 
-    var resumer = aResumer
+        callcc(nom_corutina(value))
 
-    return nil
+        
+        return value;
+    }
+
+    function retFunction(value) {
+        if (firstTime) {
+            firstTime = false;
+            aResumer(resume);
+        } else {
+            saved_continuation(value);
+        }
+    }
+
+    return (retFunction)
 }
 
+function exemple_senzill() {
+    let a = make_coroutine( function(resume, value) {
+    print("Ara estem a la corutina 'A'");
+    print("Venim de", resume(b,'A'));
+    print("Tornem a 'A'");
+    print("Venim de", resume(c,'A'));
+    });
+    let b = make_coroutine( function(resume, value) {
+    print("    Ara estem a la corutina 'B'");
+    print("    Venim de", resume(c,'B'));
+    print("    Tornem a 'B'");
+    print("    Venim de", resume(a,'B'));
+    });
+    let c = make_coroutine( function(resume, value) {
+    print("        Ara estem a la corutina 'C'");
+    print("        Venim de", resume(a,'C'));
+    print("        Tornem a 'C'");
+    print("        Venim de", resume(b,'C'));
+    });
+    // amb aquest codi evitem "complicar-nos la vida" amb
+    // problemes d'acabament quan cridem la corutina inicial
+    if (typeof(a) === 'function') {
+    a('*') // el valor '*' que passem a 'a' és irrellevant
+    }
+}
 
-
+exemple_senzill()
 
 
